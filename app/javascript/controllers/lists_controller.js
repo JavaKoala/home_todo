2fe8d05676ceded2b0410985cd1dec74a771fddb
JavaKoala <javakoala1@gmail.com>
2024-11-03 10:02:50 -0500

    Add responsiveness

--- app/javascript/controllers/lists_controller.js
+++ app/javascript/controllers/lists_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  displayLists() {
+    document.getElementById("lists-container").style = "display: block;"
   }
 }
