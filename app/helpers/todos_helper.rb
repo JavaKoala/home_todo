034147abf54d1a4ee3795ae2118172711e39545d
JavaKoala <javakoala1@gmail.com>
2025-01-07 15:31:43 -0500

    Refactor color helper

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -5,18 +5,20 @@ module TodosHelper
     todos_color_class(todo)
   end
 
-  def todos_color_class(todo) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-    case todo.due
-    when 2.days.ago..1.day.ago
-      'bg-green-100 dark:bg-green-900'
-    when 3.days.ago..2.days.ago
-      'bg-green-50 dark:bg-green-950'
-    when 4.days.ago..3.days.ago
-      'bg-red-50 dark:bg-red-950'
-    when 5.days.ago..4.days.ago
-      'bg-red-100 dark:bg-red-900'
-    else
-      'bg-red-200 dark:bg-red-800'
+  def color_ranges
+    {
+      'bg-green-100 dark:bg-green-900' => 2.days.ago..1.day.ago,
+      'bg-green-50 dark:bg-green-950' => 3.days.ago..2.days.ago,
+      'bg-red-50 dark:bg-red-950' => 4.days.ago..3.days.ago,
+      'bg-red-100 dark:bg-red-900' => 5.days.ago..4.days.ago
+    }
+  end
+
+  def todos_color_class(todo)
+    color_ranges.each do |color, range|
+      return color if range.cover?(todo.due)
     end
+
+    'bg-red-200 dark:bg-red-800'
   end
 end
